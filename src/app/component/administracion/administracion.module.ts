2b67ff6cba6a1add9d93ebf8d0838cf4f3ffc6ab
JoseCedeño <81100807+Jose-22-ced@users.noreply.github.com>
2022-08-09 11:22:46 -0500

    Arrglos

--- src/app/component/administracion/administracion.module.ts
+++ src/app/component/administracion/administracion.module.ts
@@ -3,17 +3,23 @@ import { CommonModule } from '@angular/common';
 import { BienvenidaComponent } from './bienvenida/bienvenida.component';
 import {MaterialModule} from "../../../material/material.module";
 import {RouterModule, Routes} from "@angular/router";
+import { ClientesComponent } from './clientes/clientes.component';
 
 const routes: Routes = [
   {
     path: 'bienvenida',
     component: BienvenidaComponent
+  },
+  {
+    path: 'administracionclientes',
+    component: ClientesComponent
   }
 ]
 
 @NgModule({
   declarations: [
-    BienvenidaComponent
+    BienvenidaComponent,
+    ClientesComponent
   ],
   imports: [
     CommonModule,
